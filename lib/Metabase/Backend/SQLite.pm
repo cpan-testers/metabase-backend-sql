3eb7479a0e8b24bf28aeb82e8d693288be5f3a33
David Golden <dagolden@cpan.org>
2011-08-16 16:14:24 -0400

    initial import of Metabase::Backend::SQLite

---
+++ lib/Metabase/Backend/SQLite.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package Metabase::Backend::SQLite;
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+# ABSTRACT: No abstract given for Metabase::Backend::SQLite
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Metabase::Backend::SQLite;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
